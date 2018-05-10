18d163b2adf7137ae02cf0a418c106af7e2bfafd
Nolen Johnson <johnsonnolen@gmail.com>
2018-05-10 02:31:46 -0400

    molly: Set LOCAL_PATH to Common tree
    * Without this we have to device the exact path in any reference to the common tree.

--- device.mk
+++ device.mk
@@ -13,6 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+$(LOCAL_PATH) := device/google/molly-common
+
 # Inherit Vendor
 $(call inherit-product-if-exists, vendor/google/molly/molly-vendor.mk)
 
